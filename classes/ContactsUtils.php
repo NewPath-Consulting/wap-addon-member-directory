ee5a11359f857736008589e0edb902bf6ae8c003
Natalie Brotherton <brona@umich.edu>
2021-07-27 10:43:42 -0400

    add polyfill for str_contains

--- classes/ContactsUtils.php
+++ classes/ContactsUtils.php
@@ -50,4 +50,10 @@ class ContactsUtils {
     return substr($ext, 1);
   }
 }
+
+if (!function_exists('str_contains')) {
+  function str_contains(string $haystack, string $needle) : bool {
+    return '' === $needle || false !== strpos($haystack, $needle);
+  }
+}
 ?>
