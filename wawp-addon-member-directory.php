052c2241865326ff9c6da7da0d870c60a5cf4314
Natalie Brotherton <brona@umich.edu>
2022-06-29 18:36:32 -0400

    add prefixes to constants

--- wawp-addon-member-directory.php
+++ wawp-addon-member-directory.php
@@ -33,10 +33,10 @@ use PO\classes\UserProfileShortcode;
 new ContactsAPI();
 new UserProfileShortcode();
 
-const SLUG = 'wawp-addon-member-directory'; 
-const SHOW_NOTICE_ACTIVATION = 'show_notice_activation_' . SLUG;
-const LICENSE_CHECK = 'license-check-' . SLUG;
-const NAME = 'WAWP Member Directory Addon';
+const WAWP_MEMDIR_SLUG = 'wawp-addon-member-directory'; 
+const WAWP_MEMDIR_SHOW_NOTICE_ACTIVATION = 'show_notice_activation_' . WAWP_MEMDIR_SLUG;
+const WAWP_MEMDIR_LICENSE_CHECK = 'license-check-' . WAWP_MEMDIR_SLUG;
+const WAWP_MEMDIR_NAME = 'WAWP Member Directory Addon';
 
 
 /**
@@ -49,7 +49,7 @@ function create_block_wawp_addon_member_directory_block_init() {
 		wawp_memdir_not_loaded_die();
 		return;
 	}
-	$license_valid = WAWP\Addon::instance()::has_valid_license(SLUG);
+	$license_valid = WAWP\Addon::instance()::has_valid_license(WAWP_MEMDIR_SLUG);
 	if (!$license_valid) return;
 	register_block_type_from_metadata( plugin_dir_path(__FILE__) . 'blocks/member-directory' );
 	register_block_type_from_metadata(plugin_dir_path(__FILE__) . 'blocks/member-profile');
@@ -74,7 +74,7 @@ function add_action_links($links) {
  */
 function wawp_memdir_not_loaded_notice_msg() {
 	echo "<div class='error'><p><strong>";
-	echo NAME . '</strong> requires that Wild Apricot for Wordpress is installed and activated.</p></div>';
+	echo WAWP_MEMDIR_NAME . '</strong> requires that Wild Apricot for Wordpress is installed and activated.</p></div>';
 	unset($_GET['activate']);
 	return;
 }
@@ -93,11 +93,11 @@ function wawp_memdir_not_loaded_die() {
  */
 if (class_exists('WAWP\Addon')) {
 	WAWP\Addon::instance()::new_addon(array(
-		'slug' => SLUG,
-		'name' => NAME,
+		'slug' => WAWP_MEMDIR_SLUG,
+		'WAWP_MEMDIR_NAME' => WAWP_MEMDIR_NAME,
 		'filename' => plugin_basename(__FILE__),
-		'license_check_option' => 'license-check-' . SLUG,
-		'show_activation_notice' => 'show_notice_activation_' . SLUG,
+		'license_check_option' => WAWP_MEMDIR_LICENSE_CHECK,
+		'show_activation_notice' => WAWP_MEMDIR_SHOW_NOTICE_ACTIVATION,
 		'is_addon' => 1,
 		'blocks' => array(
 			'wawp-member-addons/member-directory',
@@ -119,7 +119,7 @@ function wawp_memdir_activate() {
 		return;
 	}
 
-	WAWP\Addon::instance()::activate(SLUG);
+	WAWP\Addon::instance()::activate(WAWP_MEMDIR_SLUG);
 }
 
 /**
@@ -130,7 +130,7 @@ register_deactivation_hook(plugin_basename(__FILE__), 'wawp_memdir_deactivate');
 function wawp_memdir_deactivate() {
 	// remove from addons list
 	$addons = get_option('wawp_addons');
-	unset($addons[SLUG]);
+	unset($addons[WAWP_MEMDIR_SLUG]);
 	update_option('wawp_addons', $addons);
 }
 ?>
